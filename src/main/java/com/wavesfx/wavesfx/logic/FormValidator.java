61fe3369e5256c3c035b1849f11f01c756ebab5e
Suad Hasani <suadhasani@protonmail.com>
2019-11-30 18:22:50 +0100

    fix: form not accepting non-latin characters

--- src/main/java/com/wavesfx/wavesfx/logic/FormValidator.java
+++ src/main/java/com/wavesfx/wavesfx/logic/FormValidator.java
@@ -6,8 +6,8 @@ import java.util.stream.Stream;
 public class FormValidator {
     public static final Pattern AMOUNT_PATTERN = Pattern.compile("^|\\d+[.]?\\d*(?!.)");
     public static final Pattern MASS_TX_PATTERN = Pattern.compile("^\\w+[,]\\d+[.]?\\d*$");
-    public static final Pattern TOKEN_NAME_PATTERN = Pattern.compile("^[^-\\s][a-zA-Z0-9_\\s-]{3,}+$");
-    public static final Pattern PASSWORD_PATTERN = Pattern.compile("\\A(?=\\S*?[0-9])(?=\\S*?[a-z])(?=\\S*?[A-Z])\\S{8,}\\z");
+    public static final Pattern TOKEN_NAME_PATTERN = Pattern.compile("^[^-\\s][\\p{InBASIC_LATIN}\\s-]{3,}+$");
+    public static final Pattern PASSWORD_PATTERN = Pattern.compile("\\A(?=\\S*?\\p{Digit})(?=\\S*?\\p{IsLower})(?=\\S*?\\p{IsUpper})\\S{8,}\\z");
 
     public static boolean areValid (Boolean... booleans) {
         return Stream.of(booleans)
